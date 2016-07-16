77a2d33eabb3cd9a6f9cf0e8db64dd52a795d5ee
Souvik Das <souvikdas728@gmail.com>
2016-07-16 16:03:00 +0000

    Added form for adding Matches

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
 	get 'signout', to: 'sessions#destroy', as: 'signout'
 	resources :matches
 	root to: 'welcome#index'
-	post 'bets', to: 'matches#userbet', as: 'user_bets'
-	get 'prizes', to: 'matches#usersprizes', as: 'user_prizes'
+	post 'bets', to: 'bets#userbet', as: 'user_bets'
+	get 'prizes', to: 'bets#usersprizes', as: 'user_prizes'
 	get 'leaderboard', to: 'users#leaderboard', as: 'leaderboard'
 	get 'instructions', to: 'welcome#instructions', as: 'instructions'
 	get 'mybets', to: 'users#mybets', as: 'show_user_bets'
