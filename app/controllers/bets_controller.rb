14eb14209cc2d637d5512e216b0270079f44018e
Souvik Das <souvikdas728@gmail.com>
2016-07-23 09:59:33 +0000

    Proper MVC

--- app/controllers/bets_controller.rb
+++ app/controllers/bets_controller.rb
@@ -1,42 +1,35 @@
 class BetsController < ApplicationController
 	def userbet
-  	match_number = eval(params["id"])[:value]
-  	the_match = Match.find(match_number)
-  	team_name = eval(params["team"])[:value]
-  	amount = params[:amount].to_i
-  	if (amount <= 0)
-  		flash[:notice] = "Please enter a valid amount"
-  		redirect_to :back
-  		return
-  	end
-  	b = Bet.new
-  	b.match_id = match_number
-  	b.user_id = current_user.id
-  	b.amount = amount
-  	b.team_name = team_name.empty? ? "draw" : team_name 
-  	current_user.coins = current_user.coins - amount
-  	current_user.save!
-  	b.save!
-  	flash[:notice] = "You just betted #{amount} coins on #{team_name.empty? ? "draw" : team_name} between #{the_match.hometeam} vs #{the_match.awayteam}"
-  	redirect_to ('/')
-  end
-  
-  def usersprizes
-  	@match = Match.find(params[:id])
-  	@match.bets.each do |bet|
-  		if bet.team_name == params[:name]
-  			if params[:place] == "home"
-  				prize = bet.amount * @match.homeodds
-  			elsif params[:place] == "away"
-  				prize = bet.amount * @match.awayodds
-  			elsif params[:place] == "draw"
-  				prize = bet.amount * @match.drawodds
-  			end
-  			winner = User.find(bet.user_id)
-  			winner.coins = winner.coins + prize
-  			winner.save!
-  		end
-  	end
-  	redirect_to ('/')
-  end
+		match_number = eval(params["id"])[:value]
+		the_match = Match.find(match_number)
+		team_name = eval(params["team"])[:value]
+		amount = params[:amount].to_i
+		decider = Bet.addbet(match_number,the_match,team_name,amount, current_user)
+		if decider == true
+			flash[:notice] = "You just betted #{amount} coins on #{team_name.empty? ? "draw" : team_name} between #{the_match.hometeam} vs #{the_match.awayteam}"
+			redirect_to ('/')
+		else
+			flash[:notice] = "Please bet a valid amount"
+			redirect_to :back
+		end
+	end
+	
+	def usersprizes
+		@match = Match.find(params[:id])
+		@match.bets.each do |bet|
+		if bet.team_name == params[:name]
+			if params[:place] == "home"
+				prize = bet.amount * @match.homeodds
+			elsif params[:place] == "away"
+				prize = bet.amount * @match.awayodds
+			elsif params[:place] == "draw"
+				prize = bet.amount * @match.drawodds
+			end
+			winner = User.find(bet.user_id)
+			winner.coins = winner.coins + prize
+			winner.save!
+			end
+		end
+		redirect_to ('/')
+	end
 end
--- app/models/bet.rb
+++ app/models/bet.rb
@@ -2,4 +2,18 @@ class Bet < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :match
 	validates :amount, numericality: {greater_than: 0, message: "Please enter a valid amount"}
+	
+	def self.addbet(match_number,the_match,team_name,amount,user) 
+		if (amount <= 0)
+  		return false
+  	end
+  	b = Bet.new
+  	b.match_id = match_number
+  	b.user_id = user.id
+  	b.amount = amount
+  	b.team_name = team_name.empty? ? "draw" : team_name 
+  	user.coins = user.coins - amount
+  	user.save!
+  	b.save!
+	end
 end
